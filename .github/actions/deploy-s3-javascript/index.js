5214f55d5b81639c66e1c1e7a6217e587bc831e0
David Felipe Vanegas Ramirez <david@loka.com>
2024-09-03 16:59:02 -0500

    ci: Add job to upload files to S3

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,6 +3,20 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // 1) Get the inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const dist_folder = core.getInput('dist-folder', { required: true });
+
+    // 2) Sync the files to the S3 bucket
+    exec.exec(`aws s3 sync ${dist_folder} s3://${bucket} --region ${region}`)
+        .then(r => {
+            core.notice('Files synced to S3 bucket');
+        }).catch(e => {
+            core.setFailed(e);
+        }
+    );
+
     core.notice('Hello, World!');
 }
 
